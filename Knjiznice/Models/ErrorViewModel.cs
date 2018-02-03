93638c3c867289513cac605ccd9430a95e78d3cf
Ivan Delač <ivdelac@int.sedamIT.hr>
2018-02-03 09:54:42 +0100

    Add project files.

---
+++ Knjiznice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Knjiznice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
